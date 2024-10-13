f148cc77f1de0197fa7fe65ef862fa7cbf0e8cec
Kevin Djidoda <118570461+kdjidoda@users.noreply.github.com>
2024-10-13 20:54:21 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matematika2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Unesite jedan cijeli i jedan decimalni broj odvojene razmakom:");
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(' ');
+
+            int x = Convert.ToInt32(numbers[0]);
+            double y = Convert.ToDouble(numbers[1]);
+
+            Console.WriteLine($"Cijeli broj: {x}");
+            Console.WriteLine($"Decimale: {y}");
+            Console.ReadKey();
+        }
+    }
+}
